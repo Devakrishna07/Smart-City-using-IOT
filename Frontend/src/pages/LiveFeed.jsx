684e6ee408ea44b2f2fcfc2de290d21d8a29c298
DevaKrishna07 <devakrishna1418@gmail.com>
2025-10-19 19:48:41 +0530

    updated mobile and desktop toggle

--- Frontend/src/pages/LiveFeed.jsx
+++ Frontend/src/pages/LiveFeed.jsx
@@ -5,16 +5,20 @@ import user from '../assets/icons/user.png'
 import setting from '../assets/icons/setting.png'
 
 function LiveFeed() {
-  {/* array for sidebar */}
+  {/* array for sidebar */ }
   const items = [
-    {name:"Dashboard", path:'/', icon:dash},
-    {name:"Users", path:'/',icon:user },
-    {name:"Settings", path:'/', icon:setting}
+    { name: "Dashboard", path: '/', icon: dash },
+    { name: "Users", path: '/', icon: user },
+    { name: "Settings", path: '/', icon: setting }
   ]
   return (
     <div className='w-screen min-h-screen bg-gradient-to-bl from-[#EEEEEE] to-[#91ADC8] flex flex-col items-start  justify-center '>
       {/* sidebar */}
       <SideBar items={items} />
+
+      <div className='w-full min-h-screen flex flex-col items-center justify-start'>
+          hello
+      </div>
     </div>
   )
 }
