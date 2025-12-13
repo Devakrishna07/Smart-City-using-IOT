806eaf0d0a733b8a0d96a640b5e128bbd999504e
DevaKrishna07 <devakrishna1418@gmail.com>
2025-12-13 15:55:42 +0530

    ...

---
+++ core_systems/frontend_clients/web_cients/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './Home'
+import Settings from './Settings'
+
+function Login() {
+  return (
+    <div className='w-screen min-h-screen flex items-center justify-center bg-app-gradient '>
+      login
+    </div>
+  )
+}
+
+export default Login
---
+++ core_systems/frontend_clients/web_cients/src/pages/Settings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Settings() {
+  return (
+    <div className='w-screen min-h-screen'>
+      Settings
+    </div>
+  )
+}
+
+export default Settings
