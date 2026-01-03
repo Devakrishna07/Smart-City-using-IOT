5c27821f0f0530fb870c304bdaef92a4e907733e
DevaKrishna07 <devakrishna1418@gmail.com>
2026-01-03 12:12:16 +0530

    frontend allow

--- core_systems/backend_services/django_backend/django_backend/settings.py
+++ core_systems/backend_services/django_backend/django_backend/settings.py
@@ -155,10 +155,10 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://smart-city-using-iot.vercel.app/",
+    "https://smart-city-using-iot.vercel.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
-    "https://smart-city-using-iot.vercel.app/",
+    "https://smart-city-using-iot.vercel.app",
 ]
